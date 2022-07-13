9ecb1305c41be0118f539838aec74a29e41ef040
Nhoneso <108888549+Nhoneso@users.noreply.github.com>
2022-07-13 14:18:50 +0100

    Add files via upload

--- file.js
+++ file.js
@@ -1,86 +1,193 @@
  
-const fs = require('fs');
-const PDFDocument = require('pdfkit');
+const fs = require("fs");
+const PDFDocument = require("pdfkit");
 
 function createInvoice(invoice, path) {
-	let doc = new PDFDocument({ margin: 50 });
+  let doc = new PDFDocument({ size: "A4", margin: 50 });
 
-	generateHeader(doc);
-	generateCustomerInformation(doc, invoice);
-	generateInvoiceTable(doc, invoice);
-	generateFooter(doc);
+  generateHeader(doc);
+  generateCustomerInformation(doc, invoice);
+  generateInvoiceTable(doc, invoice);
+  generateFooter(doc);
 
-	doc.end();
-	doc.pipe(fs.createWriteStream(path));
+  doc.end();
+  doc.pipe(fs.createWriteStream(path));
 }
+
 function generateHeader(doc) {
-	doc.image('logo.png', 50, 35, { width:100 })
-		.fillColor('#444444')
-		.fontSize(20)
-		.text('BOTOFFICE.LTD', 160, 37)
-		.fontSize(10)
-		.text('7, SAHEED OLA ADELEKAN', 60, 190, { align: 'left' })
-		.text(' CITY : LEKKI PHASE 1',60 , 200, { align: 'left' }) 
-		.text(' INVOICE : INV00066958', 60, 210, { align: 'left'})
-		.text(' DATE : July 7, 2022', 60, 220, { align: 'left'})
-		.moveDown();
+  doc
+    .image("logo.png", 50, 45, { width: 50 })
+    .fillColor("#444444")
+    .fontSize(20)
+    .text("BOTOFFICE.LTD.", 110, 57)
+    .fontSize(10)
+    .text("7, SAHEED OLA ADELEKAN STREET", 200, 65, { align: "right" })
+    .text("LEKKI PHASE 1", 200, 80, { align: "right" })
+    .moveDown();
 }
-function generateTableRow(doc, y, c1, c2, c3, c4, c5) {
-	doc.fontSize(10)
-		.text(c1, 50, y, {width: 90, align: 'right'})
-		.text(c2, 150, y, {width: 90, align: 'right'})
-		.text(c3, 280, y, { width: 90, align: 'right' })
-		.text(c4, 370, y, { width: 90, align: 'right' })
-		.text(c5, 0, y, { align: 'right' });
+
+function generateCustomerInformation(doc, invoice) {
+  doc
+    .fillColor("#444444")
+    .fontSize(20)
+    .text("Invoice", 50, 160);
+
+  generateHr(doc, 185);
+
+  const customerInformationTop = 200;
+
+  doc
+    .fontSize(10)
+    .text("Invoice Number:", 50, customerInformationTop)
+    .font("Helvetica-Bold")
+    .text(invoice.invoice_nr, 150, customerInformationTop)
+    .font("Helvetica")
+    .text("Invoice Date:", 50, customerInformationTop + 15)
+    .text(formatDate(new Date()), 150, customerInformationTop + 15)
+    .text("Balance Due:", 50, customerInformationTop + 30)
+    .text(
+      formatCurrency(invoice.subtotal - invoice.paid),
+      150,
+      customerInformationTop + 30
+    )
+
+    .font("Helvetica-Bold")
+    .text(invoice.shipping.name, 300, customerInformationTop)
+    .font("Helvetica")
+    .text(invoice.shipping.address, 300, customerInformationTop + 15)
+    .text(
+      invoice.shipping.city +
+        ", " +
+        invoice.shipping.state +
+        ", " +
+        invoice.shipping.country,
+      300,
+      customerInformationTop + 30
+    )
+    .moveDown();
+
+  generateHr(doc, 252);
+}
+
+function generateInvoiceTable(doc, invoice) {
+  let i;
+  const invoiceTableTop = 330;
+
+  doc.font("Helvetica-Bold");
+  generateTableRow(
+    doc,
+    invoiceTableTop,
+    "Item",
+    "Description",
+    "Unit Cost",
+    "Quantity",
+    "Line Total"
+  );
+  generateHr(doc, invoiceTableTop + 20);
+  doc.font("Helvetica");
+
+  for (i = 0; i < invoice.items.length; i++) {
+    const item = invoice.items[i];
+    const position = invoiceTableTop + (i + 1) * 30;
+    generateTableRow(
+      doc,
+      position,
+      item.item,
+      item.description,
+      formatCurrency(item.amount / item.quantity),
+      item.quantity,
+      formatCurrency(item.amount)
+    );
+
+    generateHr(doc, position + 20);
+  }
+
+  const subtotalPosition = invoiceTableTop + (i + 1) * 30;
+  generateTableRow(
+    doc,
+    subtotalPosition,
+    "",
+    "",
+    "Subtotal",
+    "",
+    formatCurrency(invoice.subtotal)
+  );
+
+  const paidToDatePosition = subtotalPosition + 20;
+  generateTableRow(
+    doc,
+    paidToDatePosition,
+    "",
+    "",
+    "Paid To Date",
+    "",
+    formatCurrency(invoice.paid)
+  );
+
+  const duePosition = paidToDatePosition + 25;
+  doc.font("Helvetica-Bold");
+  generateTableRow(
+    doc,
+    duePosition,
+    "",
+    "",
+    "Balance Due",
+    "",
+    formatCurrency(invoice.subtotal - invoice.paid)
+  );
+  doc.font("Helvetica");
 }
 
 function generateFooter(doc) {
-	doc.fontSize(
-		10,
-	).text(
-		'Payment is due within 15 days. Thank you for your business.',
-		50,
-		600,
-		{ align: 'left', width: 500 },
-	);
-}
-function generateCustomerInformation(doc, invoice) {
-	const shipping = invoice.shipping;
-
-	doc.text(`Invoice Number: ${invoice.invoice_nr}`, 60, 120, {align: 'left'}) 
-		.text(`Invoice Date: ${new Date()}`, 60, 140, { align: 'left'})
-		.text(`Balance Due: ${invoice.subtotal - invoice.paid}`, 60, 180, { align: 'left'})
-        .moveDown()
-
-		.text(shipping.name, 170, 60, )
-		.text(shipping.address, 170, 70,)
-		.text(
-			`${shipping.city}, ${shipping.state}, ${shipping.country}, ${shipping.info}`,
-			170, 80, { align: 'left'}
-			
-		)
-		.moveDown();
+  doc
+    .fontSize(10)
+    .text(
+      "Payment is due within 15 days. Thank you for your business.",
+      50,
+      780,
+      { align: "center", width: 500 }
+    );
 }
 
-
-function generateInvoiceTable(doc, invoice) {
-	let i,
-		invoiceTableTop = 330;
-
-	for (i = 0; i < invoice.items.length; i++) {
-		const item = invoice.items[i];
-		const position = invoiceTableTop + (i + 1) * 30;
-		generateTableRow(
-			doc,
-			position,
-			item.item,
-			item.description,
-			item.amount / item.quantity,
-			item.quantity,
-			item.amount,
-		);
-	}
+function generateTableRow(
+  doc,
+  y,
+  item,
+  description,
+  unitCost,
+  quantity,
+  lineTotal
+) {
+  doc
+    .fontSize(10)
+    .text(item, 50, y)
+    .text(description, 150, y)
+    .text(unitCost, 280, y, { width: 90, align: "right" })
+    .text(quantity, 370, y, { width: 90, align: "right" })
+    .text(lineTotal, 0, y, { align: "right" });
 }
+
+function generateHr(doc, y) {
+  doc
+    .strokeColor("#aaaaaa")
+    .lineWidth(1)
+    .moveTo(50, y)
+    .lineTo(550, y)
+    .stroke();
+}
+
+function formatCurrency(cents) {
+  return "$" + (cents / 100).toFixed(2);
+}
+
+function formatDate(date) {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  return year + "/" + month + "/" + day;
+}
+
 module.exports = {
-	createInvoice
+  createInvoice
 };
